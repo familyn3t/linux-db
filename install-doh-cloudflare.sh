1b626b0af6c00aa7aa69e852571a8c5c75a408f2
David ahmad effendy <bangdavid07@gmail.com>
2025-04-13 10:24:30 +0700

    Update install-doh-cloudflare.sh

--- install-doh-cloudflare.sh
+++ install-doh-cloudflare.sh
@@ -23,6 +23,7 @@ esac
 
 apt-get install -y curl
 wget https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.deb
+#https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
 dpkg -i cloudflared-stable-linux-amd64.deb 
 cloudflared --version
 useradd -r -M -s /usr/sbin/nologin -c "Cloudflared user" cloudflared
